e8c88998fd995d8837cdf0c47ccc09ba2ef9c631
Kun Wang <kun.w@hotmail.com>
2021-12-22 18:49:12 +1300

    feature: verify contracts on mainnet for premining

--- scripts/verify-contracts.js
+++ scripts/verify-contracts.js
@@ -11,28 +11,28 @@ async function main() {
 
   // await hre.run("verify:verify", {
   //   contract: "contracts/Staking.sol:Staking",
-  //   address: "0x403Badf2caba8037d9F00bbA02D59e20DB82709D",
+  //   address: "0x1C899B399998e984a3bBfB99e37BF5C692127154",
   //   constructorArguments: [
-  //     1638748800, 14515200
+  //     1640563200, 15120000
   //   ],
   // });
 
   // await hre.run("verify:verify", {
   //   contract: "contracts/CommunityVault.sol:CommunityVault",
-  //   address: "0x80957883d147376ACF8c073c4297E9DD068cd9BC",
+  //   address: "0x918ea5c939Dd25B202b46e8b5Ee54f54D623f380",
   //   constructorArguments: [
-  //     "0xD1F9fD93af2AD6F90829E8415d79973FcFB4c8be"
+  //     "0x47DA5456bC2e1ce391b645Ce80F2E97192e4976a"
   //   ],
   // });
 
   await hre.run("verify:verify", {
     contract: "contracts/PlugYieldFarm.sol:PlugYieldFarm",
-    address: "0x941C8d9EBacEB31fC7442e10b8aa4666ABE57FCc",
+    address: "0x78Ac220f81f78780B4Ae9037598d6Ee5b096D5F5",
     constructorArguments: [
-      "0xD1F9fD93af2AD6F90829E8415d79973FcFB4c8be",
-      "0xD1F9fD93af2AD6F90829E8415d79973FcFB4c8be",
-      "0x403Badf2caba8037d9F00bbA02D59e20DB82709D",
-      "0x80957883d147376ACF8c073c4297E9DD068cd9BC"
+      "0x47DA5456bC2e1ce391b645Ce80F2E97192e4976a",
+      "0x47DA5456bC2e1ce391b645Ce80F2E97192e4976a",
+      "0x1C899B399998e984a3bBfB99e37BF5C692127154",
+      "0x918ea5c939Dd25B202b46e8b5Ee54f54D623f380"
     ],
   }); 
 }
